83c0038a1c8fbd96559a013b349f8cdc20f72800
jakkazdy <dawidlecki@outlook.com>
2015-05-07 15:29:41 +0200

    Zmienilem haslo :)

--- include/config/connect_mysql.php
+++ include/config/connect_mysql.php
@@ -6,7 +6,7 @@ class PDO_Send extends PDO {
 		$mysql_host = 'mysql03int.az.pl'; //lub jakiĹ adres: np sql.nazwa_bazy.nazwa.pl
 		$port = '3306'; //domyĹlnie jest to port 3306
 		$username = 'u1058462_dl';
-		$password = 'g}]r3El1BA';
+		$password = 'g}]r3El1BA'; //Spokojnie zmieniłem sobie hasło :)
 		$database = 'db1058462_dl'; //'produkty'
         parent::__construct('mysql:host='.$mysql_host.';dbname='.$database.';port='.$port, $username, $password );
                 parent::exec("SET NAMES utf8");
