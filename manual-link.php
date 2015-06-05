82ba019f06b3fa726c49c517cbed0405e824d1b7
jakkazdy <dawidlecki@outlook.com>
2015-06-06 01:51:47 +0200

    Delete .zip

--- manual-link.php
+++ manual-link.php
@@ -6,14 +6,14 @@ echo''
         . '<a href="index.php?url=login">Logowanie</a><br/>'
         . '<a href="index.php?url=logout">Wyloguj</a><br/>'
         . '<a href="index.php?url=register">Rejestracja</a><br/>'
-        . 'USER class==1'
+        . 'USER class==1 albo 2'
         . '<a href="index.php?url=profil">Profil</a><br/>'
         . '<a href="index.php?url=profil_e">Edytuj profil</a><br/>'
         . '<a href="index.php?url=profil_h">Zmiana hasła</a><br/>'
         
         . 'USER==1 OR USER==2'
         . '<a href="index.php?url=pack_send">PACK add/send</a><br/>'
-        . '<a href="index.php?url=pack_info">PACK info</a><br/>'
+        . '<a href="index.php?url=pack_info">PACK info co nie</a><br/>'
         . '<a href="index.php?url=pack_close">PACK close</a><br/>';
 
 echo'$_SESSION:</br>';
@@ -22,10 +22,16 @@ print_r($_SESSION);
 echo'</br>';
 echo'$view:</br>';
 print_r($view);
+echo'kolejna nowosc;l';
 echo'</br>';
 echo'ciekawe';
 echo'$p_post:</br>';
 print_r($p_post);
 echo'</br>';echo'</br>';
 ?></span>
+<<<<<<< 1001
 <h1>Myśle że ten znak się przyda - a tutaj zmiana na 1001</h1>
+=======
+<h1>Myśle że ten znak się przyda - teraz zmian dokonuje na master nie beda widoczne dla 1001</h1>
+<h1>Myśle że ten znak się przyda - a tutaj zmiana na 1001</h1>
+>>>>>>> local
